442d4bbe8766770777a7142d4ff725cefac5689f
Nate <nathanael.ledrich@icloud.com>
2024-02-17 00:53:30 +0100

    feat: define and load patterns

--- gameOfLife/patterns.py
+++ gameOfLife/patterns.py
@@ -1,7 +1,25 @@
+import tomllib
 from dataclasses import dataclass
+from pathlib import Path
+
+PATTERNS_FILE = Path(__file__).parent / "patterns.toml"
 
 
 @dataclass
 class Pattern:
     name: str
     alive_cells: set[tuple[int, int]]
+
+    @classmethod
+    def from_toml(cls, name, toml_data):
+        return cls(name, alive_cells={tuple(cell) for cell in toml_data["alive_cells"]})
+
+
+def get_pattern(name, filename=PATTERNS_FILE):
+    data = tomllib.loads(filename.read_text(encoding="utf-8"))
+    return Pattern.from_toml(name, toml_data=data[name])
+
+
+def get_all_patterns(filename=PATTERNS_FILE):
+    data = tomllib.loads(filename.read_text(encoding="utf-8"))
+    return [Pattern.from_toml(name, toml_data) for name, toml_data in data.items()]
