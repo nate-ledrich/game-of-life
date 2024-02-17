e756e03758962e687bf3743b5c06bbfc083b86ff
Nate <nathanael.ledrich@icloud.com>
2024-02-17 01:29:22 +0100

    feat: CLI implementation

--- gameOfLife/views.py
+++ gameOfLife/views.py
@@ -3,6 +3,8 @@ from time import sleep
 
 from gameOfLife.grid import LifeGrid
 
+__all__ = ["CursesView"]
+
 
 class CursesView:
     def __init__(self, pattern, gen=10, frame_rate=7, bbox=(0, 0, 20, 20)):
